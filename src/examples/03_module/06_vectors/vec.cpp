441b12a2106b97eadfdbfb1578a6ee5d9d643d4c
HienVuong-1337 <hien.vuong@g.austincc.edu>
2019-09-25 19:28:28 -0500

    vector and assignment 4

--- src/examples/03_module/06_vectors/vec.cpp
+++ src/examples/03_module/06_vectors/vec.cpp
@@ -26,8 +26,11 @@ Write code for function loop_vector_w_index with a vector of int pass by value p
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
 */
+//pass by value is a copy of parameter and original value is not changed
+
 void loop_vector_w_index(std::vector<int> nums)
 {
+	nums[0] = 1000;
 	for (int i = 0; i < nums.size(); ++i)
 	{
 		std::cout << nums[i] << "\n";
@@ -40,8 +43,11 @@ Write code for function loop_vector_w_index with a vector of int pass by referen
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows : 9 10 99 5  67
 */
+//pass by reference points to original location and can change the original vector
+
 void loop_vector_w_index_ref(std::vector<int>& nums)
 {
+	nums[0] = 1000;
 	for (int i = 0; i < nums.size(); ++i)
 	{
 		std::cout << nums[i] << "\n";
@@ -64,10 +70,21 @@ void loop_vector_w_index_const(const std::vector<int>& nums)
 }
 
 
-void loop_vector_w_for_ranged(const std::vector<int>& nums)
+void loop_vector_w_for_ranged(std::vector<int>& nums)
 {
 	for (auto num : nums)
 	{
+		nums[0] = 1000;
+		std::cout << num << "\n";
+	}
+}
+
+
+void loop_vector_w_for_ranged_ref_var(std::vector<int>& nums)
+{
+	for (auto& num : nums)
+	{
+		num = 1000;
 		std::cout << num << "\n";
 	}
 }
--- src/homework/03_iteration/dna.h
+++ src/homework/03_iteration/dna.h
@@ -4,7 +4,7 @@
 Write prototype for function get_gc_content that accepts
 a const reference string parameter and returns a double
 */
-
+double get_gc_content(const std::string );
 
 /*
 Write prototype for function get_reverse_string that 
