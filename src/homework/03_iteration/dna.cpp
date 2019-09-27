9f9a10eb4122fb0983f0c46d1c8ee087464e99a6
Hien Vuong <Hien.Vuong@g.austincc.edu>
2019-09-26 22:17:14 -0500

    hw: dna

--- src/homework/03_iteration/dna.cpp
+++ src/homework/03_iteration/dna.cpp
@@ -6,8 +6,26 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string &dna)
+{
+	double gc_count = 0;
+	double dna_ch_count = 0;   
+	//double dna_ch_count = dna.length();  
+	//double dna_ch_count = dna.size();
+	double gc_content;
 
+	for (char ch : dna)
+	{
+		++dna_ch_count;
 
+		if (ch == 'C' || ch == 'G')
+		{
+			++gc_count;
+		}
+		gc_content = gc_count / dna_ch_count;
+	}
+	return gc_content;
+}
 
 
 /*
@@ -15,7 +33,17 @@ Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
 
-
+string get_reverse_string(string dna)
+{ 
+	string reverse_dna;
+	
+	for (int i = dna.size() - 1; i >= 0; i--)
+	{
+		reverse_dna = dna[i];
+		
+	}
+	return reverse_dna;
+}
 
 /*
 Write prototype for function get_dna_complement that
@@ -25,6 +53,30 @@ a. call function get_reverse_string(dna), save results to a local string variabl
 b. iterate local string variable and
     replace A with T, T with A, C with G and G with C
 c. return string
-
 */
 
+string get_dna_complement(string dna)
+{
+	string rev_dna_comp = get_reverse_string(dna);
+
+	for (char & ch : rev_dna_comp)
+	{
+		if (ch == 'A')
+		{
+			ch = 'T';
+		}
+		else if (ch == 'T')
+		{
+			ch = 'A';
+		}
+		else if (ch == 'G')
+		{
+			ch = 'C';
+		}
+		else if (ch == 'C')
+		{
+			ch = 'G';
+		}
+	}
+	return rev_dna_comp;
+}
--- src/homework/03_iteration/main.cpp
+++ src/homework/03_iteration/main.cpp
@@ -1,7 +1,10 @@
 //write include statements
+#include<iostream>
+#include "dna.h"
 
 //write using statements
-
+using std::cout;
+using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -10,7 +13,40 @@ DNA string and call either get gc content or get dna complement
 function and display the result. Program runs as long as 
 user enters a y or Y.
 */
+
 int main() 
 {
+	string dna;
+	auto menu_option = 0;
+	auto user_choice = 'y';
+
+	do
+	{
+		cout << "Enter 1 for Get GC Content, or 2 for Get DNA Complement: ";
+		cin >> menu_option;
+
+		if (menu_option == 1)
+		{
+			cout << "Enter DNA string: ";
+			cin >> dna;
+			cout << "GC Content: " << get_gc_content(dna) << "\n";
+
+		}
+		else if (menu_option == 2)
+		{
+			cout << "Enter DNA string: ";
+			cin >> dna;
+			cout << "DNA Complement: " << get_dna_complement(dna) << "\n";
+		}
+		else
+		{
+			cout << "Invalid option, please try again";
+		}
+
+		cout << "Continue? (y/n): ";
+		cin >> user_choice;
+
+	} while (user_choice == 'Y' || user_choice == 'y');
+
 	return 0;
 }
