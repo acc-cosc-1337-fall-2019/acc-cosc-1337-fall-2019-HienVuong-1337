ea173c913d66c4bba83b678680b83e8c169faa1c
Hien Vuong <Hien.Vuong@g.austincc.edu>
2019-10-07 15:36:38 -0500

    HW 6: Tic Tac Toe starter code

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,34 @@
 //h
+#include<string>
+#include<vector>
+#include<iostream>
+
+using std::string;
+using std::vector;
+using std::cout;
+
+/*
+Represents one game of tic tac toe.
+*/
+
+class TicTacToe
+{
+public:
+	bool game_over();
+	void start_game(string player);
+	void mark_board(int position);
+	string get_player() const;
+	void display_board() const;
+
+
+private:
+	void set_next_player();
+	bool check_column_win();
+	bool check_row_win();
+	bool check_diagonal_win();
+	void clear_board();
+	bool check_board_full();
+	vector<string> pegs{9, " "};  //initializer
+	string next_player;
+
+};
