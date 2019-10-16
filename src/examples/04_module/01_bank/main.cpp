ae0220a2ac8cd7e65be34b6df60cfcefaa53875c
HienVuong-1337 <hien.vuong@g.austincc.edu>
2019-10-16 18:19:02 -0500

    CW: savings account

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include "checking_account.h"
+#include "savings_account.h"
 
 
 using std::cout;
@@ -12,6 +13,10 @@ int main()
 {
 	CheckingAccount a(1500);
 	cout << a.get_balance();
+
+	SavingsAccount savings(500);
+	savings.add_interest();
+	cout << savings;
 
 	return 0;
 }
